bab737f5dd2ee147bb4af48777212347b7d2c74f
Canar <mwcaq480@marriott.com>
2018-10-27 22:35:45 -0500

    cambios

--- assignments/lesson-7/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-   //console.log("it worked");
-   
-   //document.getElementById("pNav").classList.toggle("hide");
-
-    //console.log(document.getElementById("pNav").classList);
-    document.getElementById("pNav").classList.toggle("hide"); 
-}
