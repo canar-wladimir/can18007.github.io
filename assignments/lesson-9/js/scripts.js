cc4db18eb1db4ad9b350e699dc0f1f36fc59ddb7
Canar <mwcaq480@marriott.com>
2018-11-14 09:47:17 -0500

    mas cambios

---
+++ assignments/lesson-9/js/scripts.js
@@ -0,0 +1,19 @@
+var weatherObjet = new XMLHttpRequest();
+weatherObjet.open('GET', 'http://api.openweathermap.org/data/2.5/weather?zip=77500,mx&appid=9b187cb192bbfd069044eebd166f188e&units=imperial',true);
+
+weatherObjet.send();
+weatherObjet.onload = function() {
+    var weatherInfo = JSON.parse(weatherObjet.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('place').innerHTML = weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
+
+    var iconcode = weatherInfo.weather[0].icon;
+    var icon_path = "https://openweathermap.org/img/w/"+iconcode+".png ";
+    document.getElementById('weather_icon').innerHTML = icon_path;
+
+    console.log(icon_path);
+
+} // end of the funtion
