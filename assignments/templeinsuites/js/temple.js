7939fc8e9c750c71de1c82387cab6082f147a80d
Canar <mwcaq480@marriott.com>
2018-12-07 21:32:47 -0500

    j

--- assignments/templeinsuites/js/temple.js
+++ assignments/templeinsuites/js/temple.js
@@ -1,14 +1,22 @@
-var templeObjet = new XMLHttpRequest();
+var templeObject = new XMLHttpRequest();
 /*byuObjet.open('GET', 'https://byui-cit230.github.io/weather/data/towndata.json',true);*/
-templeObjet.open('GET', '//byui-cit230.github.io/weather/data/towndata.json',true);
+templeObject.open('GET','https://can18007.github.io/assignments/templeinsuites/js/temple.json',true);
+//templeObjet.open('GET', '//byui-cit230.github.io/weather/data/towndata.json',true);
 
-templeObjet.send();
-templeObjet.onload = function() {
-    var templeInfo = JSON.parse(byuObjet.responseText);
+templeObject.send();
+templeObject.onload = function() {
+    var templeInfo = JSON.parse(templeObject.responseText);
     console.log(templeInfo);
 
-    /*document.getElementById('name').innerHTML = byuInfo.towns[0].name;
-    document.getElementById('motto').innerHTML = byuInfo.towns[0].motto;
+    document.getElementById('name').innerHTML = "<strong>Name:</strong>" + templeInfo.temples[0].name;
+    document.getElementById('address').innerHTML = templeInfo.temples[0].address + " Tel:" + templeInfo.temples[0].telephone;
+    document.getElementById('email').innerHTML = "<strong>e-mail:</strong>" + templeInfo.temples[0].email;
+    document.getElementById('email').innerHTML = "<strong>Services:</strong>" + templeInfo.temples[0].services;
+    document.getElementById('email').innerHTML = "<strong>History:</strong>" + templeInfo.temples[0].history;
+    document.getElementById('email').innerHTML = "<strong>services:</strong>" + templeInfo.temples[0].services;
+
+
+    /*
     document.getElementById('yearFounded').innerHTML = "<strong>Year Founded:</strong>" + byuInfo.towns[0].yearFounded;
     document.getElementById('currentPopulation').innerHTML = "<strong>Population:</strong>" + byuInfo.towns[0].currentPopulation;
     document.getElementById('averageRainfall').innerHTML = "<strong>Annual Rain Fall:</strong>" + byuInfo.towns[0].averageRainfall;
@@ -24,5 +32,5 @@ templeObjet.onload = function() {
     document.getElementById('yearFounded3').innerHTML = "<strong>Year Founded:</strong>" + byuInfo.towns[6].yearFounded;
     document.getElementById('currentPopulation3').innerHTML = "<strong>Population:</strong>" + byuInfo.towns[6].currentPopulation;
     document.getElementById('averageRainfall3').innerHTML = "<strong>Annual Rain Fall:</strong>" + byuInfo.towns[6].averageRainfall;
-
-} // end of the funtion*/
+*/
+} // end of the funtion
