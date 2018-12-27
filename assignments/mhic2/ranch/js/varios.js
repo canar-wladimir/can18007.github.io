05e1985ec4d70fe7f1e5ac4ba7b4d4f628ea32e2
Canar <mwcaq480@marriott.com>
2018-12-27 12:26:36 -0500

    asfd

--- assignments/mhic2/ranch/js/varios.js
+++ assignments/mhic2/ranch/js/varios.js
@@ -14,7 +14,7 @@ request.onload = function() {
 
 function showTours(jsonObj) {
     var tours = jsonObj['tours'];
-
+    //section 
     for (var i = 0; i < tours.length; i++) {
         var myh2tour = document.createElement('h2');
         var myptour = document.createElement('p');
@@ -22,10 +22,12 @@ function showTours(jsonObj) {
         myaside.className = "aside";
         var mydiv = document.createElement('div')
         
-        myh2tour.textContent = tours[i].toursDiagram + tours[i].toursType;
+        myh2tour.textContent = tours[i].toursDigram + tours[i].toursType;
         myptour.textContent =tours[i].description;
 
         var toursfotos = tours[i].fotos;
+
+        //myaside
         for (var j = 0; j < toursfotos.length; j++) {
             var path = 'images/';
             var images = document.createElement('img');
@@ -34,9 +36,43 @@ function showTours(jsonObj) {
             myaside.appendChild(images);   
         }
 
+        
+        console.log(tourh3);
+        //mydiv
+        var tourh3 = tours[i].tours;
+        for (var j = 0; j < tourh3.length; j++) {
+            var mydiv_h3 = document.createElement('h3');
+            var mydiv_photo = document.createElement('div');
+            var mydiv_contain = document.createElement('div');
+            var mydiv_price = document.createElement('div');
+            var mydiv_form = document.createElement('div');
+            var path = 'images/';
+            var images = document.createElement('img');
+            images.setAttribute("src",path+tourh3[j].mainpic);
+            images.setAttribute("alt",tourh3[j].mainpic);
+
+            console.log(tourh3[j].name);
+            mydiv_h3.textContent = tourh3[j].name;
+            mydiv_photo.appendChild(images)
+
+            //mydiv_contain
+            var tourcontain = tourh3[j].contains;
+            for (var k = 0; k < tourcontain.length; k++) {
+                mydiv_contain.textContent = tourcontain[k]
+
+                   
+            }
+
+            mydiv.appendChild(mydiv_h3);
+            mydiv.appendChild(mydiv_photo);
+            mydiv.appendChild(mydiv_contain);
+            
+        }
+
         section.appendChild(myh2tour);
         section.appendChild(myptour);
         section.appendChild(myaside);
+        section.appendChild(mydiv);
 
 
         
